fb9bc4e9dbbc3b1a619f32dd0cf37a1fa693f6db
Nithesh <selvarajselvaraj9995@gmail.com>
2026-03-12 21:24:23 +0530

    Add files via upload

---
+++ js folder/test.js
@@ -0,0 +1,23 @@
+//swap two numbers without using third variable
+let a=10;
+let b=20;
+
+
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(a);
+console.log(b);
+
+
+// converting minutes to seconds
+let  minutes=30;
+let seconds=minutes*60;
+console.log(seconds);
+
+//getting  if a number is even or odd
+
+let number=12345;
+console.log(number%2==0);
+
+// today task finding the 
