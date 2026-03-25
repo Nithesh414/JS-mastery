dbbb0ce75726b8bc3c78d7db11bc5b36cab163f9
Nithesh <selvarajselvaraj9995@gmail.com>
2026-03-25 21:45:56 +0530

    Update script.js

--- js day-6/script.js
+++ js day-6/script.js
@@ -1,8 +1,6 @@
     let yes = document.getElementById('yes');
     let no = document.getElementById('no');
     let button = document.getElementById('submit');
-
-    // Add an event listener to the button to execute the logic when clicked
     button.addEventListener('click', function() {
         if (yes.checked) {
             alert('super');
