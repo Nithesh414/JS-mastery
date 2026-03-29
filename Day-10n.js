960f3e7f878d8d6acfa005856fa8561457ceae2f
Nithesh <selvarajselvaraj9995@gmail.com>
2026-03-29 22:29:05 +0530

    Create Day-10n.js

---
+++ Day-10n.js
@@ -0,0 +1,17 @@
+
+/*
+for sub array for getting the target by adding the elements of subarray*/
+let input=[1,2,3,7,5]
+let out=[]
+let target=12
+for(let i=0;i<input.length;i++){
+    for(j=i+1;j<input.length;j++){
+        for(k=j+1;k<input.length;k++){
+            if((input[i]+input[j]+input [k])==target){
+                out.push(input[i], input [j],input[k])
+            }
+        }
+    }
+}
+
+console.log(out)
