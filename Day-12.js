4262a0417532f0e37f3742b064dd6cba399ba0be
Nithesh <selvarajselvaraj9995@gmail.com>
2026-03-31 22:13:35 +0530

    Update Day-12.js

--- Day-12.js
+++ Day-12.js
@@ -11,3 +11,19 @@ while(num > 0){
 }
 
 console.log(sum === temp ? "Yes" : "No")
+
+/*Largest Digit*/
+
+
+let num = 59382
+let max = 0
+
+while(num > 0){
+    let d = num % 10
+    if(d > max){
+        max = d
+    }
+    num = Math.floor(num / 10)
+}
+
+console.log(max) // 9
