25d6727224d6559b2341c2489c95ca13c9f5332c
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-12 20:17:37 +0530

    Update Day-10n.js

--- Day-10n.js
+++ Day-10n.js
@@ -5,7 +5,7 @@ let input=[1,2,3,7,5]
 let out=[]
 let target=12
 for(let i=0;i<input.length;i++){
-   
+    for(j=i+1;j<input.length;j++){
         for(k=j+1;k<input.length;k++){
             if((input[i]+input[j]+input [k])==target){
                 out.push(input[i], input [j],input[k])
