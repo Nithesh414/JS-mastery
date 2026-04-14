94cbccf71811a8fe229ee01599be0718e1a2af4b
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-14 22:07:22 +0530

    Update and rename Day-.js to Day-15.js

--- Day-15.js
+++ Day-15.js
@@ -18,3 +18,16 @@ function isPalindrome(arr) {
 
 let input = [1,2,3,3,2,1];
 console.log(isPalindrome(input));
+
+
+/* palindrome in recursion*/
+function isPalindrome(arr, left, right) {
+    if (left >= right) return true;
+
+    if (arr[left] !== arr[right]) return false;
+
+    return isPalindrome(arr, left + 1, right - 1);
+}
+
+let input = [1,2,3,3,2,1];
+console.log(isPalindrome(input, 0, input.length - 1));
