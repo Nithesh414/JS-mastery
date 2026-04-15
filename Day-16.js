d6f13e7618a767fa0f7fb02226ae8879e900490e
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-15 22:27:27 +0530

    Update Day-16.js

--- Day-16.js
+++ Day-16.js
@@ -8,3 +8,16 @@ for(let i=0;i<input.length;i++){
     }
 }
 console.log(result)
+
+/*without using extra array*/
+let input = [3,2,2,3];
+let val = 3;
+
+for(let i = 0; i < input.length; i++){
+    if(input[i] == val){
+        input.splice(i,1);
+        i--;
+    }
+}
+
+console.log(input);
