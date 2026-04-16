e21c356342ee894b4f5d786cf66453e947d04bb5
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-16 21:55:44 +0530

    Improve sliding window sum calculation
    Refactor sliding window logic for clarity.

--- Day-17.js
+++ Day-17.js
@@ -1,11 +1,11 @@
+
+
+/*finding the substring sum of 15*/
+
 let arr = [4,2,1,7,8];
 let k = 2;
-
-// Step 1: first window
 let sum = arr[0] + arr[1];
 let max = sum;
-
-// Step 2: slide window
 for (let i = k; i < arr.length; i++) {
     sum = sum - arr[i - k] + arr[i];
     max = Math.max(max, sum);
