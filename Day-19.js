cd8f7f8d68a8503e32316aa77cc593eb5966cc15
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-18 23:53:04 +0530

    Add function to calculate difference between max and min

---
+++ Day-19.js
@@ -0,0 +1,15 @@
+let arr = [3, 7, 2, 9];
+
+let max = arr[0];
+let min = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+}
+
+console.log(max - min);
