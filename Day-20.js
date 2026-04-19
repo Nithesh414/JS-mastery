e22fa2cb781322c019d3384f4d28e2cbf6638512
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-19 22:10:19 +0530

    Create Day-20.js

---
+++ Day-20.js
@@ -0,0 +1,8 @@
+let input= [1,2,4,5]
+for(let i=0;i<input.length-1;i++){
+    if(input[i]+1!==input[i+1]){
+        console.log(input[i]+1)
+    }
+}
+
+/* finding the missing number in the sequence */
