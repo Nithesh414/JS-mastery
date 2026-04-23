3add8f38e58e99f2e3d0053502a354608de06ad5
Nithesh <selvarajselvaraj9995@gmail.com>
2026-04-23 22:05:32 +0530

    Update Day-20.js

--- Day-20.js
+++ Day-20.js
@@ -22,3 +22,20 @@ for(let i = 0; i < input.length; i++){
         }
     }
 }
+
+
+var singleNumber = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        let count = 0;
+        
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] === nums[j]){
+                count++;
+            }
+        }
+        
+        if(count === 1){
+            return nums[i];
+        }
+    }
+};
