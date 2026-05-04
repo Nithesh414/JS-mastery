4c79d8b26730d1b4d3833b227d287d613da57d09
Nithesh <selvarajselvaraj9995@gmail.com>
2026-05-04 21:54:16 +0530

    Update Day-21.js

--- Day-21.js
+++ Day-21.js
@@ -3,3 +3,23 @@
 for(let i=1;i<=6;i++){
     console.log("*".repeat(i))
 }
+
+
+
+let input = "][][";
+let stack = [];
+let imbalance = 0;
+
+for (let ch of input) {
+    if (ch === '[') {
+        stack.push(ch);
+    } else {
+        if (stack.length > 0) {
+            stack.pop();
+        } else {
+            imbalance++;
+        }
+    }
+}
+
+console.log("Swaps needed:", Math.ceil(imbalance / 2));
